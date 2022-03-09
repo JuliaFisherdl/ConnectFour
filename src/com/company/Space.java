7599dc3f2aa14dfaf09e72396094a50cacaad4fc
juliafisher <julia.fisher@detroitlabs.com>
2022-03-09 10:58:21 -0500

    Completed space class

---
+++ src/com/company/Space.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Space {
+    private char space;
+    private int taken;
+
+    public Space() {
+        space = '.';
+        taken = 0;
+    }
+
+    public char getSpace() {
+        return space;
+
+    }
+
+    public int getTaken() {
+        return taken;
+    }
+
+    public void setMove(int play) {
+        taken = play;
+        if (play == 1)
+            space = 'X';
+        else
+            space = 'O';
+
+    }
+
+}
